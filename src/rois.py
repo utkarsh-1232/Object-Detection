8284a89b4bfd05da25ab5c4fa8cd1d28e1e017d8
utkarsh-1232 <utkarsh.avb.singh@gmail.com>
2025-03-05 12:51:42 +0000

    include torchvision.ops.box_iou

--- src/rois.py
+++ src/rois.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
-from torchvision.transforms.functional import crop, resize
+from torchvision.ops import box_iou
+from torchvision.transforms.functional import resized_crop
 
 @dataclass
 class ROIs:
@@ -15,27 +16,10 @@ class ROIs:
     def __post_init__(self):
         self.ids = torch.full((self.boxes.shape[0],), 0, dtype=torch.long)
 
-def get_ious(rois, bboxes, epsilon=1e-5):
-    rois = torch.cat([rois[:,:2], rois[:,:2]+rois[:,2:]], dim=1)
-    bboxes = torch.cat([bboxes[:,:2], bboxes[:,:2]+bboxes[:,2:]], dim=1)
-
-    inter_xmin = torch.max(rois[:, None, 0], bboxes[None, :, 0])
-    inter_ymin = torch.max(rois[:, None, 1], bboxes[None, :, 1])
-    inter_xmax = torch.min(rois[:, None, 2], bboxes[None, :, 2])
-    inter_ymax = torch.min(rois[:, None, 3], bboxes[None, :, 3])
-
-    inter_w = (inter_xmax-inter_xmin).clamp(min=0)  # Ensure non-negative width
-    inter_h = (inter_ymax-inter_ymin).clamp(min=0)  # Ensure non-negative height
-    intersection = inter_w*inter_h
-    
-    area1 = (rois[:, 2]-rois[:, 0])*(rois[:, 3]-rois[:, 1])
-    area2 = (bboxes[:, 2]-bboxes[:, 0])*(bboxes[:, 3]-bboxes[:, 1])
-    union = area1[:, None]+area2[None, :]-intersection
-    
-    return intersection/(union+epsilon)
-
 def annotate_rois(rois, bboxes, ids, cat_thresh=0.3):
-    ious = get_ious(rois.boxes, bboxes)
+    rois.boxes = torch.cat([rois.boxes[:,:2], rois.boxes[:,:2]+rois.boxes[:,2:]], dim=1)
+    bboxes = torch.cat([bboxes[:,:2], bboxes[:,:2]+bboxes[:,2:]], dim=1)
+    ious = box_iou(rois.boxes, bboxes)
     max_ious, max_idxs = ious.max(dim=1)
     valid_mask = max_ious>cat_thresh
     rois.ids[valid_mask] = ids[max_idxs[valid_mask]]
@@ -51,7 +35,7 @@ def annotate_rois(rois, bboxes, ids, cat_thresh=0.3):
     return rois
 
 class ObjectDataset(Dataset):
-    def __init__(self, df, extract_rois, cat_thresh=0.3, crop_size=(224,224)):
+    def __init__(self, df, extract_rois, cat_thresh=0.3, crop_size=(112,112)):
         self.df = df.groupby('image_id').agg(list).reset_index()
         self.extract_rois = extract_rois
         self.crop_size = crop_size
@@ -68,7 +52,7 @@ class ObjectDataset(Dataset):
         img_path, bboxes, ids = agg_df.loc[idx]
         img = Image.open(img_path)
         img = self.img_tfms(img)
-        bboxes = torch.tensor(bboxes, dtype=torch.float16)
+        bboxes = torch.tensor(bboxes, dtype=torch.float32)
         ids = torch.tensor(ids, dtype=torch.long)
 
         rois = self.extract_rois(img)
@@ -76,9 +60,8 @@ class ObjectDataset(Dataset):
         crops = []
         for box in rois.boxes:
             x_min, y_min, w, h = box.int().tolist()
-            crop_img = crop(img, y_min, x_min, h, w)
-            crops.append(crop_img)
-        crops = [resize(crop_img, self.crop_size) for crop_img in crops]
+            cropped_img = resized_crop(img, top=y_min, left=x_min, height=h, width=w, size=self.crop_size)
+            crops.append(cropped_img)
         crops = torch.stack(crops)
 
         return {'crops':crops, 'rois':rois}
