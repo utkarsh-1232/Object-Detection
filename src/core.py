105e7c256fdd48fade6c773b85b093e0ca60ce70
utkarsh-1232 <utkarsh.avb.singh@gmail.com>
2025-03-05 08:22:53 +0000

    add json loader

--- src/core.py
+++ src/core.py
@@ -3,47 +3,41 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
 
-from torch import tensor
-from torch.utils.data import Dataset
+class JSONLoader:
+    def __init__(self, data_folder):
+        data_folder = Path(data_folder)
+        self.ann_folder = data_folder/'annotations'
+        self.img_folder = data_folder/'images'
+        self.id2img = {}
 
-path = Path('data')
-with open(path/'annotations/train.json', 'r') as f:
-    data = json.load(f)
+    def load_train(self):
+        with open(self.ann_folder/'train.json', 'r') as f:
+            data = json.load(f)
+        df = pd.DataFrame(data['annotations'])
+        self.id2img.update({d['id']:self.img_folder/d['file_name'] for d in data['images']})
+        # fix the category ids and update the dataframe
+        cat_id2id = {d['id']:i+1 for i, d in enumerate(data['categories'])}
+        df.category_id = df.category_id.replace(cat_id2id)
+        self.id2label = {i+1:d['name'] for i, d in enumerate(data['categories'])}
+        self.id2label[0] = 'background'
+        return df
 
-id2img = {d['id']:f'{path}/train_images/{d['file_name']}' for d in data['images']}
-cat_id2id = {d['id']:i+1 for i, d in enumerate(data['categories'])}
-id2label = {i+1:d['name'] for i, d in enumerate(data['categories'])}
-id2label[0] = 'background'
+    def load_test(self):
+        with open(self.ann_folder/'test.json', 'r') as f:
+            data = json.load(f)
+        df = pd.DataFrame(data['annotations'])
+        self.id2img.update({d['id']:self.img_folder/d['file_name'] for d in data['images']})
+        return df
 
-def show_bbs(img, boxes, labels=None):
+def show_bbs(img, bboxes, ids=None):
     draw = ImageDraw.Draw(img)
-    for i, box in enumerate(boxes):
-        x_min, y_min, w, h = box
+    if ids:
+        labels = [loader.id2label[id] for id in ids]
+    for i, bbox in enumerate(bboxes):
+        x_min, y_min, w, h = bbox
         draw.rectangle(((x_min, y_min), (x_min+w, y_min+h)), outline="green", width=3)
-        if labels:
+        if ids: 
             draw.text((x_min, y_min-10), labels[i], fill="white")
     plt.imshow(img)
     plt.axis('off')
     plt.show()
-
-class ObjectDataset(Dataset):
-    def __init__(self, df, tfms=None):
-        self.df = df.groupby('image_id').agg(list).reset_index()
-        self.tfms = tfms
-
-    def __len__(self):
-        return len(self.df)
-        
-    def __getitem__(self, idx):
-        img_id, boxes, ids = [*self.df.loc[idx]]
-        img = Image.open(id2img[img_id])
-        if self.tfms:
-            img = self.tfms(img)
-        boxes, ids = tensor(boxes), tensor(ids)
-        return img, boxes, ids
-
-    def show(self, idx):
-        img_id, boxes, ids = [*self.df.loc[idx]]
-        img = Image.open(id2img[img_id])
-        labels = [id2label[id] for id in ids]
-        show_bbs(img, boxes, labels)
