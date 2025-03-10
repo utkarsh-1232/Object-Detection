6c3c33bf6e5b620332359bfa0e7206b78506510f
utkarsh-1232 <utkarsh.avb.singh@gmail.com>
2025-03-10 11:45:29 +0000

    add dataset

--- src/rcnn.py
+++ src/rcnn.py
@@ -1,16 +1,29 @@
-from src.rois import ROIs
 import torch
 import torch.nn as nn
-from torchvision import models
-from selectivesearch import selective_search
+from torch.utils.data import Dataset
+from torchvision import transforms, models
+from torchvision.transforms.functional import resized_crop
 
-def extract_rois_ss(img):
-    img_area = img.shape[1]*img.shape[2]
-    _, regions = selective_search(img.permute((1,2,0)), scale=200, min_size=100)
-    rois = torch.tensor([r['rect'] for r in regions])
-    sizes = torch.tensor([r['size'] for r in regions])
-    mask = (sizes>0.05*img_area) & (sizes<img_area)
-    return ROIs(rois[mask, :])
+class RCNNDataset(Dataset):
+    def __init__(self, img_paths, rois, roi_ids, offsets, crop_size=(224,224)):
+        self.img_paths, self.rois, self.roi_ids, self.offsets = img_paths, rois, roi_ids, offsets
+        self.crop_size = crop_size
+        self.img_tfms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.img_paths[idx]).convert('RGB')
+        img = self.img_tfms(img)
+        _, H, W = img.shape
+        offset = self.offsets[idx]/torch.tensor([W,H,W,H])
+        x_min, y_min, w, h = self.rois[idx].int().tolist()
+        crop = resized_crop(img, top=y_min, left=x_min, height=h, width=w, size=self.crop_size)
+        return crop, self.roi_ids[idx], offset
 
 class RCNN(nn.Module):
     def __init__(self, n_classes):
@@ -23,12 +36,12 @@ class RCNN(nn.Module):
         self.img_encoder.classifier = nn.Sequential()
 
         self.cls_head = nn.Linear(encode_dim, n_classes)
-        self.cls_loss = nn.CrossEntropyLoss()
+        self.cls_loss_func = nn.CrossEntropyLoss()
         self.reg_head = nn.Sequential(
-             nn.Linear(encode_dim, 512), nn.ReLU(),
-             nn.Linear(512, 4), nn.Sigmoid(),
+            nn.Linear(encode_dim, 512), nn.ReLU(),
+            nn.Linear(512, 4), nn.Tanh(),
         )
-        self.reg_loss = nn.L1Loss()
+        self.reg_loss_func = nn.MSELoss()
 
     def forward(self, crops):
         features = self.img_encoder(crops)
@@ -36,11 +49,12 @@ class RCNN(nn.Module):
         bbox = self.reg_head(features)
         return probs, bbox
 
-    def calc_loss(self, preds, ids, offsets, beta=0.1):
+    def calc_loss(self, preds, ids, offsets, beta=0.2):
         probs, bbox = preds
-        cls_loss = self.cls_loss(probs, ids)
+        cls_loss = self.cls_loss_func(probs, ids)
         mask = ids!=0
         bbox, offsets = bbox[mask], offsets[mask]
-        reg_loss = self.reg_loss(bbox, offsets) if len(mask)>0 else torch.tensor(0.0, requires_grad=True)
-
+        reg_loss = self.reg_loss_func(bbox, offsets) if len(mask)>0 else torch.tensor(0.0, requires_grad=True)
+    
+        print(cls_loss, reg_loss)
         return beta*cls_loss + (1-beta)*reg_loss
