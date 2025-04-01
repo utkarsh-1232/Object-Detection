13a6f4fce57b68fabcd9d743c90075b78f0ce89e
utkarsh-1232 <utkarsh.avb.singh@gmail.com>
2025-04-01 05:44:15 +0000

    change dataset structure

--- src/rcnn.py
+++ src/rcnn.py
@@ -1,57 +1,52 @@
-import io, sys, json
 from tqdm.notebook import tqdm
 tqdm.pandas()
 import pandas as pd
-from src.core import load_data
+from src.core import load_data, perform_nms, calc_mAP
 from src.rois import apply_offsets, get_annotated_rois
 
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 from torchvision.io import read_image, ImageReadMode
-import torchvision.transforms.v2 as v2
-from torchvision import models, ops
+from torchvision import models
 
 from fastai.vision.all import DataLoaders, Metric
-from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
 
 class RCNNDataset(Dataset):
-    def __init__(self, ann_file, imgs_folder, df_sample=1, class_sampler=None,
-                 crop_size=(224,224), tfms=None, **ann_kwargs):
-        self.crop_size = crop_size
-        self.tfms = tfms
-        if self.tfms is None:
-            self.tfms = v2.Compose([
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ])
+    def __init__(self, ann_file, imgs_folder, tfms, imgs_frac=1, **ann_kwargs):
         df, self.id2label, self.id2img = load_data(ann_file, imgs_folder)
-        df = df.sample(frac=df_sample)
+        df = df.sample(frac=imgs_frac)
+        
         res = df.progress_apply(get_annotated_rois, axis=1, id2img=self.id2img, **ann_kwargs)
         self.img_ids = torch.cat([row[0].repeat(row[1].shape[0]) for row in res])
         self.rois = torch.cat([row[1] for row in res])
         self.roi_ids = torch.cat([row[2] for row in res])
         self.offsets = torch.cat([row[3] for row in res])
 
-        self.idxs = torch.tensor(range(len(self.roi_ids)))[:, None]
-        if class_sampler: self.idxs = class_sampler(self.roi_ids)
+        self.tfms = tfms
+        self.idxs = torch.arange(len(self.roi_ids))[:, None]
+            
+    def sample_rois(self, sampler):
+        self.idxs = sampler(self.roi_ids)
 
-    def __len__(self): return len(self.idxs)
+    def __len__(self):
+        return len(self.idxs)
 
     def __getitem__(self, idx):
         idx = self.idxs[idx].item()
         img_id = self.img_ids[idx]
         img = read_image(self.id2img[img_id.item()], mode=ImageReadMode.RGB)
-        img = self.tfms(img)
-        x_min, y_min, w, h = self.rois[idx].int().tolist()
-        crop = v2.functional.resized_crop(img, y_min, x_min, h, w, self.crop_size)
+        x, y, w, h = self.rois[idx].int().tolist()
+        crop = img[:, y:y+h, x:x+w]
         
-        return crop, img_id, self.rois[idx], self.roi_ids[idx], self.offsets[idx]
+        return self.tfms(crop), img_id, self.rois[idx], self.roi_ids[idx], self.offsets[idx]
 
-def get_dls(train_ds, valid_ds, bs=64):
+def get_dls(train_ds, valid_ds, bs=64, train_sampler=None):
+    if train_sampler:
+        train_ds.sample_rois(train_sampler)
     train_dl = DataLoader(train_ds, batch_size=bs, shuffle=True, pin_memory=True)
     valid_dl = DataLoader(valid_ds, batch_size=bs, shuffle=False, pin_memory=True)
+    
     dls = DataLoaders(train_dl, valid_dl)
     dls.n_inp = 1
     return dls
@@ -87,41 +82,22 @@ def reg_loss(preds, *targs):
     loss = torch.tensor(0.0, requires_grad=True)
     mask = roi_ids!=0
     if torch.sum(mask)>0:
-        loss = nn.L1Loss()(pred_offsets[mask], offsets[mask])
+        loss = nn.SmoothL1Loss()(pred_offsets[mask], offsets[mask])
     return loss
 
 def cls_loss(preds, *targs):
     return nn.CrossEntropyLoss()(preds[0], targs[2])
 
-def detn_loss(preds, *targs):
-    return cls_loss(preds, *targs) + reg_loss(preds, *targs)
+def detn_loss(preds, *targs, beta=1):
+    return cls_loss(preds, *targs) + beta*reg_loss(preds, *targs)
 
-def perform_nms(row, thresh=0.5):
-    ids, bbs, scores = row[['category_id','bbox','score']]
-    ids, bbs, scores = [torch.tensor(l) for l in [ids, bbs, scores]]
-    ids, bbs, scores = [t[ids!=0] for t in [ids, bbs, scores]]
-    boxes = torch.cat([bbs[:,:2], bbs[:,:2]+bbs[:,2:]-1], dim=1)
-    idxs = ops.batched_nms(boxes, scores, ids, thresh)
-    row[['category_id','bbox','score']] = [t[idxs].tolist() for t in [ids, bbs, scores]]
-    return row
-
-def calc_map(gt_path, pred_path):
-    with io.StringIO() as buf:
-        save_stdout = sys.stdout
-        sys.stdout = buf  # Redirect standard output
-        coco_gt = COCO(gt_path)
-        coco_pred = coco_gt.loadRes(pred_path)
-        cocoEval = COCOeval(coco_gt, coco_pred, 'bbox')
-        cocoEval.evaluate(); cocoEval.accumulate(); cocoEval.summarize()
-        sys.stdout = save_stdout  # Restore standard output
-    return cocoEval.stats[0]
-
-class mAP(Metric):
+class mAPMetric(Metric):
     def __init__(self, gt_path, pred_path, nms_thresh=0.5):
         self.gt_path, self.pred_path, self.nms_thresh = gt_path, pred_path, nms_thresh
         self.reset()
         
-    def reset(self): self.df_list = []
+    def reset(self):
+        self.df_list = []
 
     def accumulate(self, learn):
         probs, pred_offsets = learn.pred
@@ -143,5 +119,6 @@ class mAP(Metric):
         agg_df = pred_df.groupby('image_id').agg(list).reset_index()
         nms_df = agg_df.apply(perform_nms, axis=1, thresh=self.nms_thresh)
         exp_df = nms_df.explode(nms_df.columns.drop('image_id').tolist())
+        exp_df.dropna(how='any', inplace=True)
         exp_df.to_json(self.pred_path, orient='records')
-        return calc_map(self.gt_path, self.pred_path)
+        return calc_mAP(self.gt_path, self.pred_path)
